b275553f9d69a09f55b5154fc45754dbbdbc81a8
Kush Patel <55899517+kp673@users.noreply.github.com>
2023-05-30 02:32:16 -0400

    category item  styles as component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,15 +1,15 @@
-import "./category-item.styles.scss";
+import { BackgroundImage, Body, CategoryContainer } from "./category-item.styles";
 
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className="category-container">
-      <div className="background-image" style={{ backgroundImage:`url(${imageUrl})`}} />
-      <div className="category-body-container">
+    <CategoryContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </CategoryContainer>
   );
 }
 
