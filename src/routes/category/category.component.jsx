06c7d92b18ce92e2e25b129f65e15f1861aeb3b2
Kush Patel <55899517+kp673@users.noreply.github.com>
2023-05-30 01:09:26 -0400

    category styles as component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
 import { CategoriesContext } from '../../contexts/categories.context';
-import './category.styles.scss';
+import { CategoryContainer } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
@@ -13,15 +13,14 @@ const Category = () => {
   useEffect(() => {
     console.log(categoriesMap[category])
     setProducts(categoriesMap[category]);
-    console.log(products)
   }, [categoriesMap, category])
   
   return (
-    <div className='category-container'>
+    <CategoryContainer>
       {products &&
         products.map((product) => <ProductCard key={product.id} product={product}/>)
       }
-    </div>
+    </CategoryContainer>
   );
 
 }
