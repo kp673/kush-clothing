455dbe9bac7c9c779340f11681754c57a096af17
Kush Patel <55899517+kp673@users.noreply.github.com>
2023-05-30 00:47:11 -0400

    navbar as styled

--- src/routes/nav-bar/navBar.component.jsx
+++ src/routes/nav-bar/navBar.component.jsx
@@ -7,32 +7,32 @@ import { CartContext } from "../../contexts/cart.context";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import "./navigation.styles.scss"
+import { LogoContainer, NavLinks, NavigationContainer, NavLink } from"./navBar.styles";
 
 const NavBar = () => {
   const { currentUser } = useContext(UserContext)
    const {cartDropdown} = useContext(CartContext)
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className='logo-container' to='/'>
+      <NavigationContainer>
+        <LogoContainer to='/'>
           <NavLogo className='logo'/>
-        </Link>
+        </LogoContainer>
         
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        <NavLinks>
+          <NavLink to='/shop'>
             Shop
-          </Link>
+          </NavLink>
           {!currentUser ?
-            <Link className='nav-link' to='/authenticate'>
+            <NavLink  to='/authenticate'>
               Sign in
-            </Link> :
-            <span className='nav-link' onClick={signOutUser}>Sign Out</span>
+            </NavLink> :
+            <NavLink as='span' onClick={signOutUser}>Sign Out</NavLink>
           }
           <CartIcon on/>
-        </div>
+        </NavLinks>
         {cartDropdown && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet/>
     </Fragment>
   );
