4f02ecd8497cd2cbeae78cac48ef10a840839e55
Kush Patel <55899517+kp673@users.noreply.github.com>
2023-05-30 20:49:16 -0400

    user reducer complete

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,20 +1,41 @@
-import { createContext, useEffect, useState, useMemo } from "react";
-import { useNavigate } from "react-router-dom";
-import { onAuthStateChangedListner, createUserDocument } from "../utils/firebase/firebase.utils";
+import { createContext, useEffect, useReducer } from "react";
+// import { useNavigate } from "react-router-dom";
+import { createUserDocument, onAuthStateChangedListner } from "../utils/firebase/firebase.utils";
 
 //actual value to access
 export const UserContext = createContext({
   currentUser: null,
-  setCurrentUser: ()=>null
-  
+  setCurrentUser: ()=>{}
 });
 
+export const USER_ACTIONS = {
+  setCurrentUser: 'SET_CURRENT_USER'
+}
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTIONS.setCurrentUser:
+      return {
+        ...state,
+        currentUser: payload
+      }
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = useMemo(() => ({ currentUser, setCurrentUser }), [currentUser, setCurrentUser]);
-  const navigate = useNavigate();
-  const goToHome = () => navigate('/')
-  
+  const [ {currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  // const navigate = useNavigate();
+  const setCurrentUser = ((user) => {
+    dispatch({ type: USER_ACTIONS.setCurrentUser, payload: user })
+  })
+  const value = {currentUser, setCurrentUser}
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListner((user) => {
@@ -26,12 +47,5 @@ export const UserProvider = ({ children }) => {
     });
     return unsubscribe;
   });
-
-  useEffect(() => {
-    if (currentUser) {
-      goToHome();
-    }
-  }, [currentUser])
-
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
